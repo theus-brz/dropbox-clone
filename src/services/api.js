9ad0a8e49bce39737118a9686a653a11adddf909
Matheus Barbosa <37550596+TheusBrz@users.noreply.github.com>
2019-04-26 10:37:50 -0300

    Update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://omnistack-backend-brz.herokuapp.com"
+  baseURL: "https://omnistack6-backend-brz.herokuapp.com"
 });
 
 export default api;
